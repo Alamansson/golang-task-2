a19ef011183e89fc328592e0eb232fca9b6e026d
Akyl <94756078+Alamansson@users.noreply.github.com>
2022-05-08 19:29:06 +0600

    Code review

--- pkg/account.go
+++ pkg/account.go
@@ -1,13 +1,12 @@
 package pkg
 
 import (
-	"bytes"
 	"encoding/json"
 	"io"
 	"net/http"
 	"os"
 	"time"
-
+	"bytes"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"github.com/sirupsen/logrus"
@@ -16,70 +15,39 @@ import (
 
 func getAccount(c *gin.Context) {
 	client := http.Client{Timeout: 5 * time.Second}
-	request, err := http.NewRequest(http.MethodGet, "https://online.moysklad.ru/api/remap/1.2/entity/employee", http.NoBody)
-
-	if err != nil {
-		c.Status(http.StatusServiceUnavailable)
-		return
-	}
-
-	err = godotenv.Load()
-	if err != nil {
-		logrus.Fatal("Error loading .env file")
-	}
-
-	request.SetBasicAuth(os.Getenv("authLogin"), os.Getenv("authPassword"))
-	defer request.Body.Close()
-
+	request := BasicAuth(http.MethodGet, "", nil)
 	response, err := client.Do(request)
-
 	if err != nil {
 		logrus.Fatal(err)
 		return
 	}
 
 	resBody := io.Reader(response.Body)
-
-	// var responseObject interface{} // как в Документаций
-
-	var responseObject struct {
-		data map[string][]map[string]string
+	
+	// var responseObject interface{} // Выдача данных как в документаций мойСлад
+	var responseObject map[string][]map[string]string // только данные аккаунтов
+	error := json.NewDecoder(resBody).Decode(&responseObject)
+	if error == nil {
+		logrus.Fatal(err)
 	}
-
-	json.NewDecoder(resBody).Decode(&responseObject.data)
-	c.JSON(200, responseObject.data)
+	c.JSON(200, responseObject)
 }
 
 
-
 func deleteAccount(c *gin.Context) {
-	
 	client := http.Client{Timeout: 5 * time.Second}
 	id := c.Param("id")
 
-	request, err := http.NewRequest(http.MethodDelete, "https://online.moysklad.ru/api/remap/1.2/entity/employee/"+ id, http.NoBody)
+	req := BasicAuth(http.MethodDelete, id, nil)
 
-	if err != nil{
-		c.Status(http.StatusServiceUnavailable)
-		return
-	}
-	err = godotenv.Load()
-	if err != nil {
-		logrus.Fatal("Error loading .env file")
-	}
-
-	request.SetBasicAuth(os.Getenv("authLogin"), os.Getenv("authPassword"))
-
-	response, err := client.Do(request)
-
+	response, err := client.Do(req)
 	if err != nil {
 		logrus.Fatal(err)
 		return
 	}
-	defer response.Body.Close()
+	response.Body.Close()
 	c.JSON(200, gin.H{"message":"Successfully deleted"})
 }
-
 
 
 func createAccount(c *gin.Context) {
@@ -89,59 +57,39 @@ func createAccount(c *gin.Context) {
 	c.Bind(&account)
 
 	postBody, err := json.Marshal(account)
-
-	request, err := http.NewRequest(http.MethodPost, "https://online.moysklad.ru/api/remap/1.2/entity/employee", bytes.NewBuffer(postBody))
 	if err != nil {
-		c.Status(http.StatusServiceUnavailable)
+		logrus.Fatal(err)
 		return
 	}
 
-	err = godotenv.Load()
-	if err != nil {
-		logrus.Fatal("Error loading .env file")
-	}
+	request := BasicAuth(http.MethodPost, "", postBody)
+	request.Body.Close()
 
-	request.SetBasicAuth(os.Getenv("authLogin"), os.Getenv("authPassword"))
-	defer request.Body.Close()
 	request.Header.Add("Content-Type", "application/json")
-
 	response, err := client.Do(request)
 	if err != nil {
 		logrus.Fatal(err)
 		return
 	}
-	
-	defer response.Body.Close()
-
+	response.Body.Close()
 	c.JSON(200, gin.H{"message":"Successfully created"})
 }
 
 func updateAccount(c *gin.Context) {
 	client := http.Client{Timeout: 5 * time.Second}
 	id := c.Param("id")
+
 	var account interface{}
 	c.Bind(&account)
-	postBody, err := json.Marshal(account)
+
+	updateBody, err := json.Marshal(account)
 	if err != nil {
 		logrus.Fatal(err)
 		return
 	}
 
-	request, err := http.NewRequest(http.MethodPut, "https://online.moysklad.ru/api/remap/1.2/entity/employee/" + id, bytes.NewBuffer(postBody))
-
-	if err != nil {
-		c.Status(http.StatusServiceUnavailable)
-		return
-	}
-
-	err = godotenv.Load()
-	if err != nil {
-		logrus.Fatal("Error loading .env file")
-	}
-
-	request.SetBasicAuth(os.Getenv("authLogin"), os.Getenv("authPassword"))
-	
-	defer request.Body.Close()
+	request := BasicAuth(http.MethodPut, id, updateBody)
+	request.Body.Close()
 	request.Header.Add("Content-Type", "application/json")
 
 	response, err := client.Do(request)
@@ -150,7 +98,24 @@ func updateAccount(c *gin.Context) {
 		return
 	}
 	
-	defer response.Body.Close()
-
+	response.Body.Close()
 	c.JSON(200, gin.H{"message":"Successfully updated"})
 }
+
+
+func BasicAuth(method, id string, body []uint8) *http.Request {
+	bodyPost := bytes.NewBuffer(body)
+	request, err := http.NewRequest(method, "https://online.moysklad.ru/api/remap/1.2/entity/employee/" + id, bodyPost)
+	if err != nil {
+		logrus.Fatal(err)
+	}
+	err = godotenv.Load()
+	if err != nil {
+		logrus.Fatal("Error loading .env file")
+	}
+	request.SetBasicAuth(os.Getenv("authLogin"), os.Getenv("authPassword"))
+	defer request.Body.Close()
+	return request
+}
+
+
